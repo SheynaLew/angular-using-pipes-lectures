9d2761ea93ec6eddb68a064c8a58d678b35ae114
Jenny Lewis-Gandy <jenny.lewis-gandy@voxpopme.com>
2023-01-04 15:36:37 +0000

    246 - parametrizing a custom pipe

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -5,9 +5,9 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 
 export class ShortenPipe implements PipeTransform {
-  transform(value: any) {
-    if (value.length > 10) {
-      return value.substr(0, 10) + "...";
+  transform(value: any, length: number) {
+    if (value.length > length) {
+      return value.substr(0, length) + "...";
     };
     return value;
   };
